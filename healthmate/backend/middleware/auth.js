8613eefb81445b53361ec39d48dc120844f94aa9
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-06 10:54:40 +0500

    Update auth.js

--- healthmate/backend/middleware/auth.js
+++ healthmate/backend/middleware/auth.js
@@ -1,11 +1,10 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const protect = async (req, res, next) => {
-  try {
+  try{
     let token;
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      token = req.headers.authorization.split(' ')[1];
-    }
+      token = req.headers.authorization.split(' ')[1];}
     
     if (!token && req.cookies.token) {
       token = req.cookies.token;
