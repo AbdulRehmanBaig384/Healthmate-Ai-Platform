51ea1bb881302e04034c71ec59a6487f05826ad0
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-13 01:53:34 +0500

    Update User.js

--- healthmate/backend/models/User.js
+++ healthmate/backend/models/User.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose');
-const bcrypt =require('bcryptjs');
+const bcrypt=require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-  name: {
+const userSchema=new mongoose.Schema({
+  name:{
     type: String,
     required: [true, 'Name is required'],
     trim: true,
