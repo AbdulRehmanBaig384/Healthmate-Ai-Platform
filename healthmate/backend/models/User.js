c7e3abf231229fcb54e1ad4c97851e2107a86dda
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-13 01:57:38 +0500

    Update User.js

--- healthmate/backend/models/User.js
+++ healthmate/backend/models/User.js
@@ -33,18 +33,18 @@ const userSchema=new mongoose.Schema({
   isVerified:{
     type:Boolean,
     default:false},
-  lastLogin: {
-    type: Date,
-    default: Date.now
+  lastLogin:{
+    type:Date,
+    default:Date.now
   }
 }, {
-  timestamps: true
+  timestamps:true
 });
 // Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
-  try {
+  try{
     const salt = await bcrypt.genSalt(12);
     this.password = await bcrypt.hash(this.password, salt);
     next();
