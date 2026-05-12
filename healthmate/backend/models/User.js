41160ada313850650e4de3a5ed9a11261e05beb4
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-13 01:59:35 +0500

    Update User.js

--- healthmate/backend/models/User.js
+++ healthmate/backend/models/User.js
@@ -21,9 +21,9 @@ const userSchema=new mongoose.Schema({
     minlength:[6,'Password must be at least 6 characters'],
     select: false
   },
-  avatar: {
-    type: String,
-    default: null
+  avatar:{
+    type:String,
+    default:null
   },
   language:{
     type:String,
