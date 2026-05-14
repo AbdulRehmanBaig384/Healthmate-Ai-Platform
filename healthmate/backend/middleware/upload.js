2fcebbe7d47c7788c17fd7946206906c39d3affd
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-15 02:19:17 +0500

    Fix formatting of import statements in upload.js

--- healthmate/backend/middleware/upload.js
+++ healthmate/backend/middleware/upload.js
@@ -1,5 +1,5 @@
-const multer = require('multer');
-const cloudinary = require('cloudinary').v2;
+const multer=require('multer');
+const cloudinary=require('cloudinary').v2;
 const {CloudinaryStorage}=require('multer-storage-cloudinary');
 const path = require('path');
 const fs = require('fs');
