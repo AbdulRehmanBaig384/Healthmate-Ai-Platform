9afdace21bd3ca328dd4cec29f33b0e5449b9edb
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-17 12:57:18 +0500

    Update authController.js

--- healthmate/backend/controllers/authController.js
+++ healthmate/backend/controllers/authController.js
@@ -6,14 +6,14 @@ const register=async(req,res)=>{
     const existingUser = await User.findOne({ email });
     if(existingUser){
       return res.status(400).json({
-        success: false,
-        message: 'User already exists with this email'
+        success:false,
+        message:'User already exists with this email'
       });}
-    const user = await User.create({
+    const user=await User.create({
       name,
       email,
       password,
-      language: language || 'en'
+      language:language||'en'
     });
     sendTokenResponse(user, 201, res);
   } catch (error) {
